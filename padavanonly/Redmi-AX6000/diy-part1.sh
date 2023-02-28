28f21c3a90a57531e6acb7d0283b99f071fb2c2d
lhy0315cn <65433261+lhy0315cn@users.noreply.github.com>
2023-02-28 15:12:16 +0800

    Update diy-part1.sh

--- padavanonly/Redmi-AX6000/diy-part1.sh
+++ padavanonly/Redmi-AX6000/diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
-echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git kenzok https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git sansun888 https://github.com/sansun888/OpenAppFilter' >>feeds.conf.default
 #sed -i '$a src-git kenzok https://github.com/kenzok8/openwrt-packages' feeds.conf.default
